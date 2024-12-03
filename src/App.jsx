cd87731b83aab57521a0e69dd3f84ad9d797d002
Travis Schellenberg <travis.schellenberg@gmail.com>
2024-12-03 15:25:08 -0800

    Added react router dom, going to setup basic sign in/ sign up flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 // import { useState } from 'react'
 import { useRoutes } from 'react-router-dom'
 import './App.css'
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import Movies from './pages/Movies';
 
 function App() {
   // Using react router to handle routing through site
   const element = useRoutes([
     {
-      path:'/HomePage',
-      element:''
+      path:'/',
+      element: <HomePage />
+    },
+    {
+      path:'/Signin',
+      element: <Signin />
+    },
+    {
+      path:'/Signup',
+      element: <Signup />
+    },
+    {
+      path:'/Movies',
+      element: <Movies />
+    },
+    {
+      path: "*",
+      element: <NotFoundPage />
     }
+
   ]);
 
 }
