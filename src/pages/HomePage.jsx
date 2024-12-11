9901f5d1fb48846500d6f030f3e61199941423dc
Travis Schellenberg <travis.schellenberg@gmail.com>
2024-12-11 12:41:34 -0800

    updated homepage banner

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -46,23 +46,19 @@ const HomePage = () => {
       {/* Background Image Section */}
       <div className="background-image">
         <div className="content">
-          <h1 className="heading">Welcome to Webflix</h1>
-          <p className="description">Make an account to browse our collection of films!</p>
+          <h1 className="heading">Welcome to Webflix!</h1>
+          <p className="description">Limited movies, TV shows, and more</p>
+          <h4 className='sub-heading'>Sign up free. Cancel anytime.</h4>
 
-          {/* Page Links */}
+          {/* Page Link */}
           <div className="mt-16 border-t border-gray-800 pt-8 text-center lg:text-left">
+            
             <div className="text-base font-semibold text-gray-400">
+              Ready to watch?   
               <Link to="/signup" className="text-white hover:underline">
                 Sign Up Here
-              </Link>{' '}
-              |{' '}
-              <Link to="/terms-and-conditions" className="text-white hover:underline">
-                Terms and Conditions
-              </Link>{' '}
-              |{' '}
-              <Link to="/privacy-policy" className="text-white hover:underline">
-                Privacy Policy
               </Link>
+            
             </div>
           </div>
         </div>
