20f8b72343eaf0c1d92adcf6075458c2315e5bcf
Сукач Артем <artemgood@MacBook-Pro-Sukac.local>
2024-12-11 22:07:46 -0800

    search implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { AppBar, Box, Button, Menu, MenuItem, Toolbar } from '@mui/material';
+import { AppBar, Box, Button, Menu, MenuItem, Toolbar, IconButton, TextField, Typography, Grid } from '@mui/material';
 import { signOut } from 'firebase/auth';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { auth } from '../firebaseConfig';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 import logo from '../assets/logo.png';
 import userlogo from '../assets/user.png';
 
@@ -11,6 +13,12 @@ const Navbar = ({ favoritesRef, watchlaterRef }) => {
     const location = useLocation();
     const [user, setUser] = useState(null);
     const [anchorEl, setAnchorEl] = useState(null);
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [filteredMovies, setFilteredMovies] = useState([]);
+    const [genres, setGenres] = useState({});
+
+    const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -18,6 +26,24 @@ const Navbar = ({ favoritesRef, watchlaterRef }) => {
         });
         return () => unsubscribe();
     }, []);
+
+    useEffect(() => {
+        const fetchGenres = async () => {
+            try {
+                const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+                const data = await response.json();
+                const genreMap = {};
+                data.genres.forEach((genre) => {
+                    genreMap[genre.name.toLowerCase()] = genre.id;
+                });
+                setGenres(genreMap);
+            } catch (error) {
+                console.error('Error fetching genres:', error);
+            }
+        };
+
+        fetchGenres();
+    }, [API_KEY]);
 
     const handleSignout = async () => {
         try {
@@ -48,6 +74,7 @@ const Navbar = ({ favoritesRef, watchlaterRef }) => {
             navigate('/movies');
         }
     };
+
     const handleUserLogoHover = (event) => {
         setAnchorEl(event.currentTarget);
     };
@@ -72,7 +99,39 @@ const Navbar = ({ favoritesRef, watchlaterRef }) => {
         }, 100);
     };
 
-    // List of public routes where login/signup buttons should appear
+    const handleSearchOpen = () => {
+        setIsSearchOpen(true);
+    };
+
+    const handleSearchClose = () => {
+        setIsSearchOpen(false);
+        setSearchQuery('');
+        setFilteredMovies([]);
+    };
+
+    const handleSearchChange = async (event) => {
+        const query = event.target.value.toLowerCase();
+        setSearchQuery(query);
+
+        if (query.trim() === '') {
+            setFilteredMovies([]);
+            return;
+        }
+
+        const genreId = genres[query];
+        const url = genreId
+            ? `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genreId}`
+            : `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            setFilteredMovies(data.results || []);
+        } catch (error) {
+            console.error('Error fetching search results:', error);
+        }
+    };
+
     const publicRoutes = ['/', '/signin', '/signup', '/privacy-policy', '/terms-and-conditions'];
     const isPublicRoute = publicRoutes.includes(location.pathname);
 
@@ -112,9 +171,9 @@ const Navbar = ({ favoritesRef, watchlaterRef }) => {
                                     backgroundColor: 'transparent',
                                     textTransform: 'none',
                                     fontWeight: 'bold',
+                                    marginRight: '10px',
                                     boxShadow: 'none',
-                                }}
-                            >
+                                }}                            >
                                 Login
                             </Button>
                             <Button
@@ -126,53 +185,149 @@ const Navbar = ({ favoritesRef, watchlaterRef }) => {
                                     backgroundColor: 'transparent',
                                     textTransform: 'none',
                                     fontWeight: 'bold',
-                                    marginRight: '40px',
+                                    marginRight: '10px',
                                     boxShadow: 'none',
                                 }}
-                
                             >
                                 Sign Up
                             </Button>
                         </>
                     ) : (
                         <>
-                            <Box>
-                                <img
-                                    src={userlogo}
-                                    alt="User Logo"
+                            <IconButton onClick={handleSearchOpen} style={{ position: 'relative', top: '-16px', left: '-15px' }}>
+                                <SearchIcon sx={{ fontSize: '32px', color: '#4262BE' }} />
+                            </IconButton>
+                            
+                            {isSearchOpen && (
+                                <Box
                                     style={{
-                                        height: 45,
-                                        cursor: 'pointer',
-                                        marginRight: '25px',
-                                        marginTop: '5px',
-                                    }}
-                                    onMouseEnter={handleUserLogoHover}
-                                />
-
-                                <Menu
-                                    anchorEl={anchorEl}
-                                    open={Boolean(anchorEl)}
-                                    onClose={handleCloseDropdown}
-                                    MenuListProps={{
-                                        onMouseLeave: handleCloseDropdown,
+                                        position: 'fixed',
+                                        top: 0,
+                                        left: 0,
+                                        width: '100%',
+                                        height: '100%',
+                                        backgroundColor: 'rgba(0, 0, 0, 0.9)',
+                                        color: 'white',
+                                        zIndex: 1300,
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        justifyContent: 'flex-start',
+                                        padding: '20px',
                                     }}
                                 >
-                                    <MenuItem onClick={() => handleNavigation('/account')}>
-                                        My Account
-                                    </MenuItem>
-                                    <MenuItem onClick={() => handleDivNavigation('favorites')}>
-                                    Favorite Movies
-                                    </MenuItem>
-                                    <MenuItem onClick={() => handleDivNavigation('watchlater')}>
-                                    Watch Later
-                                    </MenuItem>
-                                    <MenuItem onClick={handleSignout}>Sign Out</MenuItem>
-                                </Menu>
-                            </Box>
+                                    <IconButton
+                                        style={{ alignSelf: 'flex-end', color: 'white' }}
+                                        sx={{ position: 'relative', left: '-30px' }} 
+                                        onClick={handleSearchClose}
+                                    >
+                                        <CloseIcon />
+                                    </IconButton>
 
-                                    </>
-                                )}
-                            </Box>
+                                    <TextField
+                                        autoFocus
+                                        variant="outlined"
+                                        placeholder="Search by title or genre..."
+                                        value={searchQuery}
+                                        onChange={handleSearchChange}
+                                        InputProps={{
+                                            style: { color: 'white', fontSize: '1.5rem' },
+                                        }}
+                                        style={{
+                                            marginBottom: '20px',
+                                            width: '80%',
+                                        }}
+                                    />
+
+                                    <Box style={{ overflowY: 'auto', width: '80%' }}>
+                                        {filteredMovies.length > 0 ? (
+                                            <Grid container spacing={3}>
+                                                {filteredMovies.map((movie) => (
+                                                    <Grid item xs={6} sm={4} md={3} key={movie.id}>
+                                                        <Box
+                                                            style={{
+                                                                display: 'flex',
+                                                                flexDirection: 'column',
+                                                                alignItems: 'center',
+                                                                cursor: 'pointer',
+                                                            }}
+                                                            onClick={async () => {
+                                                                try {
+                                                                    const response = await fetch(
+                                                                        `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${API_KEY}`
+                                                                    );
+                                                                    const data = await response.json();
+                                                                    const videoId = data.results[0]?.key;
+
+                                                                    if (videoId) {
+                                                                        navigate('/watch-trailer', {
+                                                                            state: { videoId, movieTitle: movie.title },
+                                                                        });
+                                                                    } else {
+                                                                        alert('Trailer not available!');
+                                                                    }
+                                                                } catch (error) {
+                                                                    console.error('Error fetching video data:', error);
+                                                                    alert('Failed to fetch trailer!');
+                                                                }
+                                                            }}
+                                                        >
+                                                            <img
+                                                                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                                                                alt={movie.title}
+                                                                style={{
+                                                                    width: '100%',
+                                                                    height: 'auto',
+                                                                    borderRadius: '5px',
+                                                                }}
+                                                            />
+                                                            <Typography variant="body2" style={{ marginTop: '10px', textAlign: 'center' }}>
+                                                                {movie.title}
+                                                            </Typography>
+                                                        </Box>
+                                                    </Grid>
+                                                ))}
+                                            </Grid>
+                                        ) : (
+                                            <Typography>No results found</Typography>
+                                        )}
+                                    </Box>
+                                </Box>
+                            )}
+
+                            <img
+                                src={userlogo}
+                                alt="User Logo"
+                                style={{
+                                    height: 45,
+                                    cursor: 'pointer',
+                                    marginRight: '25px',
+                                    marginTop: '15px',
+                                }}
+                                onMouseEnter={handleUserLogoHover}
+                            />
+                            <Menu
+                                anchorEl={anchorEl}
+                                open={Boolean(anchorEl)}
+                                onClose={handleCloseDropdown}
+                                MenuListProps={{
+                                    onMouseLeave: handleCloseDropdown,
+                                }}
+                            >
+                                <MenuItem onClick={() => handleNavigation('/account')}>
+                                    My Account
+                                </MenuItem>
+                                <MenuItem onClick={() => handleDivNavigation('favorites')}>
+                                    Favorite Movies
+                                </MenuItem>
+                                <MenuItem onClick={() => handleDivNavigation('watchlater')}>
+                                    Watch Later
+                                </MenuItem>
+                                <MenuItem onClick={handleSignout}>Sign Out</MenuItem>
+                            </Menu>
+                        </>
+                    )}
+                </Box>
             </Toolbar>
         </AppBar>
     );
